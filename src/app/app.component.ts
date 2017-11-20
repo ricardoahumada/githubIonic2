d5f0cfd5ffd878c59a05e219ca4ac1cbc824044c
ricardoahumada <ricardo@enmotionvalue.com>
2017-11-20 22:55:51 +0100

    project update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,22 +1,47 @@
-import { Component } from '@angular/core';
-import { Platform } from 'ionic-angular';
+import { Component, ViewChild } from '@angular/core';
+import { Platform, MenuController, Nav } from 'ionic-angular';
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
 
-import { HomePage } from '../pages/home/home';
+import { UsersPage } from '../pages/users/users';
+import { ReposPage } from '../pages/repos/repos';
+import { OrganisationsPage } from '../pages/organisations/organisations';
+
 @Component({
   templateUrl: 'app.html'
 })
 export class MyApp {
-  rootPage:any = HomePage;
+  @ViewChild(Nav) nav: Nav;
 
-  constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen) {
-    platform.ready().then(() => {
+  // make UsersPage the root (or first) page
+  rootPage: any = UsersPage;
+  pages: Array<{title: string, component: any}>;
+
+  constructor(public platform: Platform,  public menu: MenuController, private statusBar:StatusBar) {
+    this.initializeApp();
+
+    // set our app's pages
+    this.pages = [
+    { title: 'Users', component: UsersPage },
+    { title: 'Repos', component: ReposPage },
+    { title: 'Organisations', component: OrganisationsPage },
+    ];
+  }
+
+  initializeApp() {
+    this.platform.ready().then(() => {
       // Okay, so the platform is ready and our plugins are available.
       // Here you can do any higher level native things you might need.
-      statusBar.styleDefault();
-      splashScreen.hide();
+      this.statusBar.styleDefault();
     });
   }
+
+  openPage(page) {
+    // close the menu when clicking a link from the menu
+    this.menu.close();
+    // navigate to the new page if it is not the current page
+    this.nav.setRoot(page.component);
+  }
+
 }
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,26 +4,42 @@ import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
 import { SplashScreen } from '@ionic-native/splash-screen';
 import { StatusBar } from '@ionic-native/status-bar';
 
+import {HttpModule} from '@angular/http';
+
 import { MyApp } from './app.component';
-import { HomePage } from '../pages/home/home';
+
+import { UsersPage } from '../pages/users/users';
+import { ReposPage } from '../pages/repos/repos';
+import { OrganisationsPage } from '../pages/organisations/organisations';
+import {UserDetailsPage } from '../pages/user-details/user-details';
+
+import { GithubUsers } from '../providers/github-users';
 
 @NgModule({
   declarations: [
     MyApp,
-    HomePage
+    UsersPage,
+    ReposPage,
+    OrganisationsPage,
+    UserDetailsPage
   ],
   imports: [
     BrowserModule,
+    HttpModule,
     IonicModule.forRoot(MyApp)
   ],
   bootstrap: [IonicApp],
   entryComponents: [
     MyApp,
-    HomePage
+    UsersPage,
+    ReposPage,
+    OrganisationsPage,
+    UserDetailsPage
   ],
   providers: [
     StatusBar,
     SplashScreen,
+    GithubUsers,
     {provide: ErrorHandler, useClass: IonicErrorHandler}
   ]
 })
